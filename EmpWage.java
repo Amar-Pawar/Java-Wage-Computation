ff48f9bb775035580502ed4cbe3183d1f4da6e9c
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-14 06:55:20 +0530

    Add:Part time employee and its wages added

--- EmpWage.java
+++ EmpWage.java
@@ -3,17 +3,25 @@ public class EmpWage {
 	System.out.println("Welcome to Employee wage Computation program");
 	//initializing variables
 	int is_Full_Time = 1;
+        int is_Part_Time = 2;
         int wage_Per_Hr = 20;
         int work_Hrs = 8;
-	//check if employee present or absent using random and calculating wages and printing
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        int part_Time_Hrs = 4;
+        int wages = 0;
+	//use random to generate random number 
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+	//using if statement to check if employee is present or not and calculate wages
         if (empCheck == is_Full_Time){
-                int wages = (wage_Per_Hr * work_Hrs);
+                wages = (wage_Per_Hr * work_Hrs);
                 System.out.println("Employee is present");
-                System.out.println("wages of the employee are " + wages);
+        }
+        else if (empCheck == is_Part_Time){
+                wages = (wage_Per_Hr * part_Time_Hrs);
+                System.out.println("Employee is part time present");
         }
         else
                 System.out.println("Employee is absent");
-
+	
+	System.out.println("wages of the employee are " + wages);
 	}
 }
