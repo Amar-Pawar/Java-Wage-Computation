aed347d2af91b4984edf6daa01b5f54217513f45
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-14 06:36:30 +0530

    Add:Employee attendance check added

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,12 @@
 public class EmpWage {
 	public static void main (String[] args) {
 	System.out.println("Welcome to Employee wage Computation program");
+	int is_Full_Time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == is_Full_Time)
+        System.out.println("Employee is present");
+        else
+        System.out.println("Employee is absent");
+
 	}
 }
