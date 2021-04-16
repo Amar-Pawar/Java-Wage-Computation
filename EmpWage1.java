55cb16115848b12d23a609061beb4f5865fa4467
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-16 22:10:11 +0530

    Refactor:Solved by creating class method

--- EmpWage1.java
+++ EmpWage1.java
@@ -6,39 +6,43 @@ public class EmpWage1 {
         public static final int month_days = 20;
         public static final int max_Hrs = 100;
 
-        public static void main (String[] args) {
+	//creating a class method
+        public static int totalWage() {
         //initializing variables
-        int days = 0;
-        int total_Hrs = 0;
-        int total_Wages = 0;
-        int empHrs = 0;
-        //loop through using while loop till condition is met
-        while (total_Hrs != max_Hrs && days != month_days) {
+        	int days = 0;
+        	int total_Hrs = 0;
+        	int empHrs = 0;
+        	//loop through using while loop till condition is met
+        	while (total_Hrs != max_Hrs && days != month_days) {
 
-                days ++;
-                int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+                	days ++;
+                	int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 
-        //use switch case to check if employee present, absent or part time present
-                switch (empCheck) {
-                        case is_Full_Time:
-                                empHrs = 8;
-                                        break;
+       			//use switch case to check if employee present, absent or part time present
+               	 	switch (empCheck) {
+                        	case is_Full_Time:
+                                	empHrs = 8;
+                                        	break;
 
-                        case is_Part_Time:
-                                empHrs = 4;
-                                        break;
+                        	case is_Part_Time:
+                                	empHrs = 4;
+                                        	break;
 
-                        default:
-                                empHrs = 0;
-                                        break;
+                        	default:
+                                	empHrs = 0;
+                                        	break;
 
-                }
-                total_Hrs += empHrs;
-                int empWage = (empHrs * wage_Per_Hr);
-                total_Wages += empWage;
+                	}
+                	total_Hrs += empHrs;
+		}
+                int empWage = (total_Hrs * wage_Per_Hr);
+                System.out.println("total wages: " + empWage);
+		return empWage;
+		
         }
-        System.out.println("total salary " + total_Wages);
+	public static void main(String[] args) {
+        totalWage();     //calling class method
+	}
 
-}
 }
 
