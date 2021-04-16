f8178997c83d65564c112fcdb7310dfb1a1815eb
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-16 11:51:14 +0530

    Add:solved by declaring class variables

--- EmpWage1.java
+++ EmpWage1.java
@@ -1,40 +1,44 @@
 public class EmpWage1 {
+	 //initializing constants that is class variables
+        public static final int is_Full_Time = 1;
+        public static final int is_Part_Time = 2;
+        public static final int wage_Per_Hr = 20;
+        public static final int month_days = 20;
+        public static final int max_Hrs = 100;
+
         public static void main (String[] args) {
-       //initializing vaiables
-	int is_Full_Time = 1;
-        int is_Part_Time = 2;
-        int wage_Per_Hr = 20;
-        int work_Hrs = 8;
-        int part_Time_Hrs = 4;
-        int month_Days = 20;
-        int wages = 0;
-        int monthly_Wages = 0;
-	//using random to generate random number
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-	//converting double into integer for switch case
-        int empcheck = (int) empCheck;
-	//initializing a string for employee attendance status
-        String status;
-	//using switch case to check if employee present, absent or part time present based on random number generated
-        switch (empcheck) {
-        case 1:
-                status = "employee is present";
-		//calculating daily wages
-                wages = (wage_Per_Hr * work_Hrs);
-                break;
-        case 2:
-                status = "employee is part time present";
-                wages = (wage_Per_Hr * part_Time_Hrs);
-                break;
-        default:
-                status = "employee is absent";
-                System.out.println(status);
-                break;
+        //initializing variables
+        int days = 0;
+        int total_Hrs = 0;
+        int total_Wages = 0;
+        int empHrs = 0;
+        //loop through using while loop till condition is met
+        while (total_Hrs != max_Hrs && days != month_days) {
+
+                days ++;
+                int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+
+        //use switch case to check if employee present, absent or part time present
+                switch (empCheck) {
+                        case is_Full_Time:
+                                empHrs = 8;
+                                        break;
+
+                        case is_Part_Time:
+                                empHrs = 4;
+                                        break;
+
+                        default:
+                                empHrs = 0;
+                                        break;
+
+                }
+                total_Hrs += empHrs;
+                int empWage = (empHrs * wage_Per_Hr);
+                total_Wages += empWage;
         }
-	//caculating monthly wages
-	monthly_Wages = (month_Days * wages);
-        System.out.println(status + " and daily wages are " + wages);
-        System.out.println("monthly wages of employee " + monthly_Wages);
+        System.out.println("total salary " + total_Wages);
+
 }
 }
 
