a76d5e53448a4dd2b59347956592cfc034a7f978
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-20 07:23:33 +0530

    add:used instance variables than parameter so get wages

--- EmpWage1.java
+++ EmpWage1.java
@@ -1,65 +1,61 @@
-public class EmpWage1 {
-         //initializing constants that is class variables
-        public static final int is_Full_Time = 1;
-        public static final int is_Part_Time = 2;
+class companyWages {
+        //initializing class variables
+        public static final int is_Full_Time=1;
+        public static final int is_Part_Time=2;
+
         private final String company;
-        private final int wage_Per_Hr;
         private final int month_days;
         private final int max_Hrs;
+        private final int wage_Per_Hr;
         private int totalEmpWage;
 
-        //creating a class method
-        public EmpWage1(String company, int wage_Per_Hr, int month_days, int max_Hrs) {
-
-                this.company = company;
-                this.wage_Per_Hr = wage_Per_Hr;
-                this.month_days = month_days;
-                this.max_Hrs = max_Hrs;
-        }
-        public void computeWage(){
-                //initializing variables
-                int days = 0;
-                int total_Hrs = 0;
-                int empHrs = 0;
-                //loop through using while loop till condition is met
-                while (total_Hrs != max_Hrs && days != month_days) {
-
-                        days ++;
-                        int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-	         //use switch case to check if employee present, absent or part time present
-                        switch (empCheck) {
-                                case is_Full_Time:
-                                        empHrs = 8;
-                                                break;
-
-                                case is_Part_Time:
-                                        empHrs = 4;
-                                                break;
-
-                                default:
-                                        empHrs = 0;
-                                                break;
-
-                        }
-                        total_Hrs += empHrs;
-                        System.out.println("Day#: " + days + " emp hrs: " +empHrs);
-                }
-                totalEmpWage = total_Hrs * wage_Per_Hr;
+        public companyWages (String company, int wage_Per_Hr, int month_days, int max_Hrs) {
+        this.company = company;
+        this.wage_Per_Hr = wage_Per_Hr;
+        this.month_days = month_days;
+        this. max_Hrs = max_Hrs;
         }
 
-        //@override
-        public String toString() {
-                return "total emp wage for company: " + company + " is: " + totalEmpWage;
+        public void computeEmpWage() {
+        //initializing variables
+        int empHrs=0;
+        int total_Hr=0;
+        int days=0;
+        while (total_Hr != max_Hrs && days < month_days) {
+                days++;
+                int empCheck=(int)Math.floor(Math.random()*10) % 3;
+                switch (empCheck) {
+                        case is_Full_Time:
+                                empHrs=8;
+                                break;
+                        case is_Part_Time:
+                                empHrs=4;
+                                break;
+                        default:
+                                empHrs=0;
+                                break;
+              }
+	         total_Hr += empHrs;
+        	  System.out.println("Day: "+days+ "Emp Hr:"+empHrs);
+ 	 }
+     	  totalEmpWage= total_Hr * wage_Per_Hr;
+	}
 
-        }
-        public static void main(String[] args) {
-                EmpWage1 DMart = new EmpWage1("DMart",20,20,100);
-                EmpWage1 Relience = new EmpWage1("Relience",30,25,80);
-                DMart.computeWage();
-                System.out.println(DMart);
-                Relience.computeWage();
-                System.out.println(Relience);
-        }
+	@Override
+  	public String toString() {
+  	return "Total Emp Wage for company: "+company+"is:"+totalEmpWage;
+  	}
+}
+public class EmpWage1 {
+public static void main(String[] args) {
 
+        companyWages DMart=new companyWages("DMart", 20, 20, 100 );
+        DMart.computeEmpWage();
+        System.out.println(DMart);
+
+        companyWages JioMart=new companyWages("JioMart", 30, 25, 80 );
+        JioMart.computeEmpWage();
+        System.out.println(JioMart);
+ }
 }
 
